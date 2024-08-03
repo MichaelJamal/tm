c3fd67e7937b3a4c9dff8ec2794312853fdbb8b2
samuelalison <samuelalison13@gmail.com>
2024-08-03 16:13:28 +0100

    added the 2 bottom sheets

--- app/TicketDetails.jsx
+++ app/TicketDetails.jsx
@@ -1,277 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React, { useEffect, useRef, useState } from 'react';
-// import { View, Text, TouchableOpacity, ScrollView, ImageBackground, StyleSheet,  Dimensions, Button, Image  } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-// import Swiper from 'react-native-swiper';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { ActivityIndicator } from 'react-native';
-// import { Icon } from 'react-native-elements';
-// const { height: screenHeight } = Dimensions.get('window');
-// import BottomSheet from 'react-native-simple-bottom-sheet';
-
-
-
-// const TicketDetails = () => {
-//   const navigation = useNavigation();
-//   const [eventData, setEventData] = useState('');
-//   const screenHeight = Dimensions.get('window').height;
-//   const panelRef = useRef(null);
-  
- 
-
-
-//   useEffect(() => {
-//     const getData = async () => {
-//       try {
-//         const result = await AsyncStorage.getItem('event');
-//         if (result) {
-//           const parsedResult = JSON.parse(result);
-//           // console.log('Event Data Retrieved:', parsedResult);
-//           setEventData(parsedResult);
-//         } else {
-//           console.log('No event data found');
-//         }
-//       } catch (error) {
-//         console.error('Failed to fetch event data from storage', error);
-//       }
-//     };
-//     getData();
-//   }, []);
-
-
-
-//   // Render slides based on slideForms
-//   const renderSlides = () => {
-//     if (!eventData.slideForms || eventData.slideForms.length === 0) {
-//       return (
-//         <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-//          <ActivityIndicator size="large" color="#007BFF" />
-//          <Text style={{ marginTop: 16, fontSize: 16, color: '#007BFF' }}>Loading tickets...</Text>
-//         </View>
-//       );
-//     } 
-
-//     return eventData.slideForms.map((slideForm, index) => (
-//       <View 
-//       key={index} 
-//       style={{  justifyContent: 'center', marginHorizontal: 34 }}
-//       >
-// <View style={{ backgroundColor: '#007BFF', 
-//   padding: 12, 
-//   borderTopLeftRadius: 8,
-//   borderTopRightRadius: 8,
-  
-//   }}>
-// <Text style={{ color: '#fff', textAlign: 'center', marginBottom: 34 }}>Verified Fan Offer</Text>
-// <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 8, paddingHorizontal: 16}}>
-//     <View style={{ alignItems: 'center' }}>
-//     <Text style={{ color: '#fff' }}>SEC</Text>
-//     <Text style={{ color: '#fff', fontSize: 24, fontWeight: 'bold' }}>{slideForm.sec}</Text>
-//     </View>
-//     <View style={{ alignItems: 'center' }}>
-//     <Text style={{ color: '#fff' }}>ROW</Text>
-//     <Text style={{ color: '#fff', fontSize: 24, fontWeight: 'bold' }}>{slideForm.row}</Text>
-//     </View>
-//     <View style={{ alignItems: 'center' }}>
-//     <Text style={{ color: '#fff' }}>SEAT</Text>
-//     <Text style={{ color: '#fff', fontSize: 24, fontWeight: 'bold' }}>{slideForm.seat}</Text>
-//     </View>
-// </View>
-// </View>
-
-// <View style={{ marginVertical: 4}} >
-
-//       <ImageBackground
-//         source={require('../assets/taylor.jpg')}
-//         style={{  justifyContent: 'center', alignItems: 'center', width: '100%', height: 240, }}
-//         resizeMode="cover"
-//       >
-//         <View style={styles.overlay} />
-        
-//         <View style={{ alignItems: 'center', position: 'absolute', padding: 16, bottom: 0 }}>
-//           <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 4, color: '#fff'}}>
-//           {eventData.artistName}  |  {eventData.event}
-//           </Text>
-//           <Text style={{ fontSize: 18, color: '#fff', marginBottom: 16}}>
-//           {eventData.date} • {eventData.venue}
-//           </Text>
-//         </View>
-//       </ImageBackground>
-
-//     </View>
-
-
-// <View style={{paddingVertical: 30}}>
-// <Text style={{ fontSize: 18, marginBottom: 16, textAlign: 'center' }}>{slideForm.numberOfSections}</Text>
-
-// </View>
-
-// <View style={{ }}>
-// <TouchableOpacity style={{ backgroundColor: '#000', padding: 12, flexDirection: 'row', 
-// alignItems: 'center', justifyContent: 'center', 
-// marginBottom: 30,
-// borderRadius: 2
-//  }}>
- 
-//  <Image
-//           style={{ width: 40, borderRadius: 4, height: 24, marginRight: 16 }} 
-//           source={require('../assets/images.jpeg')}
-//         />
-//     <Text style={{ color: '#fff' }}>Add to Apple Wallet</Text>
-// </TouchableOpacity>
-// </View>
-
-// <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginBottom: 36}}>
-//     <TouchableOpacity>
-//     <Text style={{ color: '#6c757d', fontSize: 12 }}>View Barcode</Text>
-//     </TouchableOpacity>
-//     <TouchableOpacity>
-//     <Text style={{ color: '#6c757d', fontSize: 12 }}>Ticket Details</Text>
-//     </TouchableOpacity>
-// </View>
-
-//     <View style={{height: 44, backgroundColor: '#007BFF', borderBottomRightRadius: 8, borderBottomLeftRadius: 8,  }}></View>
-//       </View>
-//     ));
-//   };
-
-
-
-//   const togglePanel = () => {
-//     if (panelRef.current) {
-//       panelRef.current.togglePanel();
-//     } else {
-//       console.error('panelRef.current is null');
-//     }
-//   };
-
-
-// return (
-// <>
-// <StatusBar
-// barStyle="dark-content" // options: 'default', 'light-content', 'dark-content'
-// backgroundColor="#333" // works on Android
-// hidden={false} // true to hide the status bar
-// translucent={false} // true to allow content to render under the status bar
-// />
-// <View style={{ flex: 1, backgroundColor: '#fff'}}>
-
-// <View style={{textAlign: 'center'}}>
-// <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 16, alignItems: 'center', backgroundColor: '#333', }}>
-// <TouchableOpacity>
-   
-//     <Text onPress={() => navigation.goBack()} style={{ fontSize: 16, color: '#fff' }} ><Icon
-//   name="close"
-//   type="EvilIcons"
-//   color="#fff"
-// /></Text>
-  
-// </TouchableOpacity>
-// <Text style={{ fontSize: 16, color: '#fff', fontWeight: 'bold'}}>My Tickets</Text>
-// <TouchableOpacity>
-//     <Text style={{ fontSize: 16, color: '#fff' }}>Help</Text>
-// </TouchableOpacity>
-// </View>
-
-// <View style={{ marginVertical: 8 }}>
-// <ScrollView>
-// <Swiper
-//           // style={{ height: '100%' }}  // Set your desired height
-//           showsPagination={true}  // Shows dots pagination
-//           dotStyle={{ backgroundColor: 'rgba(0,0,0,.2)', width: 8, height: 8, }}  // Style for dots
-//           activeDotStyle={{ backgroundColor: '#000', width: 8, height: 8 }}  // Style for active dot
-//           style={{ height: screenHeight - 144 }}
-//         >
-//           {renderSlides()}
-//  </Swiper>
-       
-//  </ScrollView>
-
-
-
-
-
-
-// <View style={{Flex: 3, flexDirection: 'row', justifyContent: 'space-between', margin: 8, marginHorizontal: 12 }}>
-//     <TouchableOpacity style={{ backgroundColor: '#007BFF', padding: 16, borderRadius: 12, flex: 1, marginRight: 8, alignItems: 'center' }} onPress={togglePanel}>
-//     <Text style={{ color: '#fff', fontWeight: 'bold' }}  >Transfer</Text>
-   
-//     </TouchableOpacity>
-//     <TouchableOpacity style={{ backgroundColor: '#E0E0E0', padding: 16, borderRadius: 12, flex: 1, marginLeft: 8, alignItems: 'center' }}>
-//     <Text style={{ color: '#fff', fontWeight: 'bold' }}>Sell</Text>
-//     </TouchableOpacity>
-// </View>
-
-// </View>
-
-
-//       </View>
-
-
-//   <BottomSheet
-//    ref={panelRef}
-//    isOpen={false}
-  
-//   lineContainerStyle={{ display: 'none' }}
-//    >
-//           <Text style={{paddingVertical: 80}}>
-//             Some random content
-//           </Text>
-//         </BottomSheet>
-     
-
-// </View>
-// </>
-// );
-// };
-
-// export default TicketDetails;
-
-
-
-
-// const styles = StyleSheet.create({
-//     eventImage: {
-//         width: '100%',
-//         height: 250,
-//       },
-//       // overlay: {
-//       //   position: 'absolute',
-//       //   top: 0,
-//       //   left: 0,
-//       //   width: '100%',
-//       //   height: screenHeight / 3, // Covering half of the screen height
-//       //   backgroundColor: 'rgba(0, 0, 0, 0.3)', // Adjust the opacity as needed
-//       // },
-//       overlay: {
-//     ...StyleSheet.absoluteFillObject,
-//     backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity as needed
-//   },
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
@@ -289,7 +15,7 @@ const { height: screenHeight } = Dimensions.get('window');
 const TicketDetails = () => {
   const navigation = useNavigation();
   const [eventData, setEventData] = useState({});
-  // const panelRef = useRef(null);
+  const panelRef = useRef(null);
   const [isPanelVisible, setIsPanelVisible] = useState(false);
   const [isPanelVisible2, setIsPanel2Visible2] = useState(false);
   const [activeSlideForm, setActiveSlideForm] = useState(eventData.slideForms ? eventData.slideForms[0] : null);
@@ -297,6 +23,7 @@ const TicketDetails = () => {
   // const [selectedSeat, setSelectedSeat] = useState(null);
   const [checked, setChecked] = useState(false);
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  const prevSlideIndexRef = useRef(0);
   
   
   const toggleCheckbox = () => setChecked(!checked);
@@ -317,6 +44,14 @@ const TicketDetails = () => {
     };
     getData();
   }, []);
+
+
+
+  useEffect(() => {
+    if (eventData.slideForms && eventData.slideForms.length > 0) {
+      setActiveSlideForm(eventData.slideForms[activeSlideIndex]);
+    }
+  }, [activeSlideIndex, eventData.slideForms]);
 
 
 
@@ -395,11 +130,25 @@ const TicketDetails = () => {
   //   ));
   // };
   
-  useEffect(() => {
-    if (eventData.slideForms && eventData.slideForms.length > 0) {
-      setActiveSlideForm(eventData.slideForms[activeSlideIndex]);
-    }
-  }, [activeSlideIndex, eventData.slideForms]);
+
+  // const handleIndexChanged = (index) => {
+  //   console.log('Prev index:', prevSlideIndexRef.current);
+  //   console.log('New index:', index);
+    
+  //   // Update the active slide index and ref
+  //   setActiveSlideIndex(index);
+  //   prevSlideIndexRef.current = index;
+  // };
+
+
+
+
+
+  // useEffect(() => {
+  //   if (eventData.slideForms && eventData.slideForms.length > 0) {
+  //     setActiveSlideForm(eventData.slideForms[activeSlideIndex]);
+  //   }
+  // }, [activeSlideIndex, eventData.slideForms]);
 
   
 
@@ -416,12 +165,13 @@ const TicketDetails = () => {
     return eventData.slideForms.map((slideForm, index) => (
       <View 
         key={slideForm.id} 
+        id={`slide-${index}`}
         style={{ justifyContent: 'center', marginHorizontal: 34 }}
       >
         {/* padding should be 12 */}
         <View style={{ backgroundColor: '#007BFF', padding: 12, borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
-        {/* marginBottom should be 34 */}
-          <Text style={{ color: '#fff', textAlign: 'center', marginBottom: 0 }}>Verified Fan Offer</Text>
+       
+          <Text style={{ color: '#fff', textAlign: 'center', marginBottom: 34 }}>Verified Fan Offer</Text>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 8, paddingHorizontal: 16 }}>
             <View style={{ alignItems: 'center' }}>
               <Text style={{ color: '#fff' }}>SEC</Text>
@@ -457,8 +207,8 @@ const TicketDetails = () => {
         </View>
 
 
-        {/* paddingVertical should be 30 */}
-        <View style={{ paddingVertical: 2 }}>
+     
+        <View style={{ paddingVertical: 30 }}>
           <Text style={{ fontSize: 18, marginBottom: 16, textAlign: 'center' }}>{slideForm.numberOfSections}</Text>
         </View>
 
@@ -495,6 +245,7 @@ const TicketDetails = () => {
   const handleBackgroundPress = () => {
     if (isPanelVisible) {
       togglePanel();
+      setChecked(false);
     }
   };
 
@@ -505,6 +256,7 @@ const TicketDetails = () => {
   const handleBackgroundPress2 = () => {
     if (isPanelVisible2) {
       togglePanel2();
+      // setChecked(false);
     }
   };
 
@@ -513,7 +265,7 @@ const TicketDetails = () => {
 
 
 
-  console.log("eventData", eventData)
+  // console.log("eventData", eventData)
 
   return (
     <>
@@ -540,26 +292,20 @@ const TicketDetails = () => {
 
           {/* HERE */}
             {/* The marginVertical should be 8 */}
-          <View style={{ marginVertical: 0 }}>
+          <View style={{ marginVertical: 8 }}>
 
 
 
             <ScrollView>
-            <Swiper
-        showsPagination={true}
-        dotStyle={{ backgroundColor: 'rgba(0,0,0,.2)', width: 8, height: 8 }}
-        activeDotStyle={{ backgroundColor: '#000', width: 8, height: 8 }}
-        style={{ height: screenHeight - 144 }}
-        onIndexChanged={(index) => {
-          setActiveSlideIndex(index);
-          setActiveSlideForm(eventData.slideForms[index]); // Immediate update
-          if (eventData.slideForms && eventData.slideForms.length > 0) {
-            setActiveSlideForm(eventData.slideForms[index]); // Optimistic update
-          }
-        }}
-      >
-        {renderSlides()}
-      </Swiper>
+              <Swiper
+              showsPagination={true}
+              dotStyle={{ backgroundColor: 'rgba(0,0,0,.2)', width: 8, height: 8 }}
+              activeDotStyle={{ backgroundColor: '#000', width: 8, height: 8 }}
+              style={{ height: screenHeight - 144 }}
+              // onIndexChanged={(index) => handleIndexChanged(index)}
+              >
+              {renderSlides()}
+              </Swiper>
             </ScrollView>
 
 
@@ -570,7 +316,6 @@ const TicketDetails = () => {
 
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 8, marginHorizontal: 12 }}>
               <TouchableOpacity style={{ backgroundColor: '#007BFF', padding: 16, borderRadius: 12, flex: 1, marginRight: 8, alignItems: 'center' }} 
-              
               onPress={togglePanel}
               >
                 <Text style={{ color: '#fff', fontWeight: 'bold' }}>Transfer</Text>
@@ -586,7 +331,7 @@ const TicketDetails = () => {
 
 
         {/* {isPanelVisibleRef.current && ( */}
-        { activeSlideForm && isPanelVisible && (
+        {activeSlideForm && isPanelVisible &&  (
           //  eventData.slideForms.map((slideForm) => (
           <TouchableWithoutFeedback onPress={handleBackgroundPress}>
             <View style={styles.overlay}>
@@ -756,7 +501,7 @@ const TicketDetails = () => {
 
       <TouchableOpacity
         style={{ }}
-        // onPress={togglePanel2}
+        onPress={togglePanel2}
       >
         <Text
           style={{
@@ -780,8 +525,10 @@ const TicketDetails = () => {
         // ))
         )}
 
+{/* Put the second sheet below */}
 
-        {/* {isPanelVisible2 && (
+        
+        {isPanelVisible2 && (
           <TouchableWithoutFeedback onPress={handleBackgroundPress2}>
              <View style={styles.overlay2}>
               <BottomSheet
@@ -888,6 +635,7 @@ const TicketDetails = () => {
       style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}
       >
         <TouchableOpacity
+        onPress={handleBackgroundPress2}
         style={{
           // marginBottom: 10,
         }}
@@ -936,9 +684,7 @@ const TicketDetails = () => {
     </BottomSheet>
         </View>
           </TouchableWithoutFeedback>
-        )} */}
-
-        
+        )}
       </View>
     </>
   );
@@ -964,11 +710,6 @@ const styles = StyleSheet.create({
 });
 
 export default TicketDetails;
-
-
-
-
-
 
 
 
